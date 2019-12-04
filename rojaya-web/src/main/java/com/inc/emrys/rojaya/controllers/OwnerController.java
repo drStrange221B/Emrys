b182c214a1ea8efe773dc318719977c6d7a4ab96
Rojaya Maharjan <rmaharjan@apple.com>
2019-12-04 00:19:13 -0500

    Adding List in Owner index page. Closes #15

--- rojaya-web/src/main/java/com/inc/emrys/rojaya/controllers/OwnerController.java
+++ rojaya-web/src/main/java/com/inc/emrys/rojaya/controllers/OwnerController.java
@@ -1,14 +1,24 @@
 package com.inc.emrys.rojaya.controllers;
 
+import com.inc.emrys.rojaya.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/owners")
 public class OwnerController {
 
-    @RequestMapping({"/owners","/owners/index","/owners/index.html"})
-    public String listOwners()
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOwners(Model model)
     {
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
