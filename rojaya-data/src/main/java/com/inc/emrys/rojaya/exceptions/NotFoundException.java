0438e7b0c1079ff75bd33348f0095be65fcbf918
Rojaya Maharjan <rojaya.mh3@gmail.com>
2020-04-13 17:15:14 -0400

    added json endpoint.

---
+++ rojaya-data/src/main/java/com/inc/emrys/rojaya/exceptions/NotFoundException.java
@@ -0,0 +1,18 @@
+package com.inc.emrys.rojaya.exceptions;
+
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(){
+        super();
+    }
+
+    public NotFoundException(String message){
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
+
